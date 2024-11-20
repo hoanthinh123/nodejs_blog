a42ec4a2cb8c1f43d4e98d56c3ee5baac0f407a1
hoanthinh123 <hoanthinh311@gmail.com>
2024-11-20 22:26:31 +0700

    template handlebars

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+const path = require('path')
+// http logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+})); // Sử dụng engine từ express-handlebars
+app.set('view engine', 'hbs'); // Chỉ định view engine là handlebars
+app.set('views', path.join(__dirname,'resources/views')) // Đảm bảo thư mục chứa các file .handlebars là 'views'
+
+app.get('/', (req, res) => {
+   res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+
+// app.get('/', (req, res) => {
+//   return res.send(`
+//     <h1 style="color:red;">Hello World!</h1>
+//     `)
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
